219cbc906a12d6af8e373f300046384772466e44
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2024-06-18 12:59:29 +0530

    Delete main directory

--- main/java/com/example/services/HelloService.java
+++
@@ -1,7 +0,0 @@
-package com.example.services;
-
-import com.example.dto.res.HelloResponse;
-
-public interface HelloService {
-    HelloResponse getHelloMessage();
-}
--- main/java/com/hellows/services/HelloService.java
+++
@@ -1,7 +0,0 @@
-package com.hellows.services;
-
-import com.hellows.dto.res.HelloResponse;
-
-public interface HelloService {
-    HelloResponse getHelloMessage();
-}
